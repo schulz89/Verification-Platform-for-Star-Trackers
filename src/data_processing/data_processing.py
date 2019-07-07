29491fd21d8c26b7a7bf0db0c392256c6201e5c4
Victor Hugo Schulz <schulz89@gmail.com>
2019-07-07 16:06:39 +0900

    Added reproduction of results test.

--- src/data_processing/data_processing.py
+++ src/data_processing/data_processing.py
@@ -23,46 +23,47 @@ from config_dut import ConfigDut
 from definitions import test_t, dut_t, grid_t
 
 class SingleTest:  # Base class
-    def __init__(self, common = ConfigCommon(), dut_list = [ConfigDut()]):
+    def __init__(self, common_list = [ConfigCommon()], dut_list = [ConfigDut()]):
         os.makedirs("config", exist_ok=True)
         self.dut_list = copy.deepcopy(dut_list)
-        self.common = copy.deepcopy(common)
-        for dut in self.dut_list:
+        self.common_list = copy.deepcopy(common_list)
+        for common, dut in zip(self.common_list, self.dut_list):
+            self.common = common
             self.dut = dut
             self.parameters()
-            if not self.exists(dut):
-                self.configure(self.dut)
-                self.run(self.dut)
-                self.save(self.dut)
+            if not self.exists(common, dut):
+                self.configure(self.common, self.dut)
+                self.run(self.common, self.dut)
+                self.save(self.common, self.dut)
         self.plot()
     def parameters (self):
         pass
     def delta_update(self, value):
         pass
-    def exists(self,dut_config):
-        directory_name = "data/" + self.common.test_identifier
+    def exists(self, common_config, dut_config):
+        directory_name = "data/" + common_config.test_identifier
         filename = directory_name + "/" + dut_config.dut_identifier + ".npy"
         return os.path.isfile(filename) # If the file already exists, skip test
-    def configure(self,dut_config):
-        self.common.generate_yml("config/common.yml")
+    def configure(self, common_config, dut_config):
+        common_config.generate_yml("config/common.yml")
         dut_config.generate_yml("config/dut.yml")
         subprocess.run(["bin/grid_database"])
-    def run(self,dut_config):
-        self.test_sel = test_t[self.common.test_name]
+    def run(self, common_config, dut_config):
+        self.test_sel = test_t[common_config.test_name]
         self.dut_sel  = dut_t[dut_config.dut_name]
         #if subprocess.call(["bin/verification_platform", str(self.dut_sel), str(self.test_sel)]) :
         if subprocess.call(["bin/verification_platform", str(self.dut_sel), str(self.test_sel)], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL) :
             exit()
-        self.data = np.genfromtxt("data/" + self.common.output_filename, comments="%", delimiter="\t")
+        self.data = np.genfromtxt("data/" + common_config.output_filename, comments="%", delimiter="\t")
         # print("Data size:" + repr(np.size(self.data,axis=0)) + "x" + repr(np.size(self.data,axis=1)))
-    def save(self,dut_config):
-        directory_name = "data/" + self.common.test_identifier
+    def save(self, common_config, dut_config):
+        directory_name = "data/" + common_config.test_identifier
         filename_base = directory_name + "/" + dut_config.dut_identifier
         os.makedirs(directory_name, exist_ok=True)
         np.save(filename_base + ".npy", self.data)    
     def plot(self):
-        for dut in self.dut_list:
-            directory_name = "data/" + self.common.test_identifier
+        for common, dut in zip(self.common_list, self.dut_list):
+            directory_name = "data/" + common.test_identifier
             filename_base = directory_name + "/" + dut.dut_identifier
             data = np.load(filename_base + ".npy")
             ratio = data[:,0] / data[:,2]
@@ -70,52 +71,52 @@ class SingleTest:  # Base class
             # plt.hist(ratio, bins = 20, facecolor = "dimgrey")
             axes = plt.gca()
             axes.set_xlim([0,1])
-            plt.xlabel(self.common.x_axis_name)
-            plt.ylabel(self.common.y_axis_name)
+            plt.xlabel(common.x_axis_name)
+            plt.ylabel(common.y_axis_name)
             plt.grid()
             plt.savefig(filename_base + ".pdf")
             # plt.show()
             plt.clf()
 
 class MultipleTests(SingleTest): # Base class
-    def exists(self,dut_config):
+    def exists(self, common_config, dut_config):
         directory_name = "data/" + self.common.test_identifier
         filename_x = directory_name + "/" + dut_config.dut_identifier + "_x.npy"
-        filename_y = directory_name + "/" + dut_config.dut_identifier + "_x.npy"
+        filename_y = directory_name + "/" + dut_config.dut_identifier + "_y.npy"
         return os.path.isfile(filename_x) and os.path.isfile(filename_y) # If the files already exists, skip test
-    def run(self,dut_config):
-        start = self.common.start
-        end = self.common.end
-        steps = self.common.steps
+    def run(self, common_config, dut_config):
+        start = common_config.start
+        end = common_config.end
+        steps = common_config.steps
         self.x = np.linspace(start,end,steps)
         self.y = np.empty(steps)
         for i in range(0,steps) :
             self.delta_update(self.x[i])
-            super().run(dut_config)
+            super().run(common_config, dut_config)
             total = np.size(self.data,axis=0)
             acc = np.sum(self.data,axis=0)
             self.y[i] = acc[4]/total
-    def save(self,dut_config):
-        directory_name = "data/" + self.common.test_identifier
+    def save(self, common_config, dut_config):
+        directory_name = "data/" + common_config.test_identifier
         filename_base = directory_name + "/" + dut_config.dut_identifier
         os.makedirs(directory_name, exist_ok=True)
         np.save(filename_base + "_x.npy", self.x)
         np.save(filename_base + "_y.npy", self.y)
     def plot(self):
-        directory_name = "data/" + self.common.test_identifier
+        directory_name = "data/" + self.common_list[0].test_identifier
         linestyles = ['-', '--', '-.', ':']
         i = 0
-        for dut in self.dut_list:
+        for common, dut in zip(self.common_list, self.dut_list):
             filename_base = directory_name + "/" + dut.dut_identifier
             x = np.load(filename_base + "_x.npy")
             y = np.load(filename_base + "_y.npy")
             plt.plot(x, y, label=dut.dut_identifier, linestyle=linestyles[i%len(linestyles)])
             i = i + 1
-        plt.xlabel(self.common.x_axis_name)
-        plt.ylabel(self.common.y_axis_name)
+        plt.xlabel(common.x_axis_name)
+        plt.ylabel(common.y_axis_name)
         plt.grid()
         plt.legend()
-        plt.savefig(directory_name + "/" + self.common.test_identifier + ".pdf")
+        plt.savefig(directory_name + "/" + self.common_list[0].test_identifier + ".pdf")
         # plt.show()
         plt.clf()
 
@@ -140,6 +141,39 @@ class TestStarTrackerPosition(MultipleTests):
     def delta_update(self,value):
         self.common.sp_position_std_dev = value
         self.common.generate_yml("config/common.yml")
+
+class TestStarTrackerPositionRPR(MultipleTests):
+    def parameters(self):
+        self.common.test_identifier = "Star Tracker Position Reproduction"
+        self.common.start = 0 # pixels
+        self.common.end = 1   # pixels
+        self.common.steps = 6
+        self.common.x_axis_name = "Additive Gaussian noise on position (pixel)"
+        self.common.y_axis_name = "Ratio of correct attitude determination"
+    def delta_update(self,value):
+        self.common.sp_position_std_dev = value
+        self.common.generate_yml("config/common.yml")
+    def plot(self):
+        directory_name = "data/" + self.common_list[0].test_identifier
+        linestyles = ['-', '--', '-.', ':']
+        i = 0
+        for common, dut in zip(self.common_list, self.dut_list):
+            filename_base = directory_name + "/" + dut.dut_identifier
+            x = np.load(filename_base + "_x.npy")
+            y = np.load(filename_base + "_y.npy")
+            plt.plot(x, y, label=dut.dut_identifier, linestyle=linestyles[i%len(linestyles)])
+            i = i + 1
+        plt.xlabel(common.x_axis_name)
+        plt.ylabel(common.y_axis_name)
+        axes = plt.gca()
+        axes.set_xlim([0,1])
+        axes.set_ylim([0.795,1.005])
+        plt.grid()
+        plt.legend()
+        plt.savefig(directory_name + "/" + self.common_list[0].test_identifier + ".pdf")
+        plt.show()
+        plt.clf()
+
 
 class TestStarTrackerMagnitude(MultipleTests):
     def parameters(self):
